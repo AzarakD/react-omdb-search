e273f45b682753b38bbca81f39d5fdd982565302
DmitryMishchenko <64646382+AzarakD@users.noreply.github.com>
2022-07-03 23:30:16 +0400

    fix api base url

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,7 +5,7 @@ import Search from "../components/Search";
 import Preloader from "../components/Preloader";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-const API_HOST = `http://www.omdbapi.com/?apikey=${API_KEY}`;
+const API_HOST = `https://www.omdbapi.com/?apikey=${API_KEY}`;
 
 export default class Main extends Component {
   state = {
